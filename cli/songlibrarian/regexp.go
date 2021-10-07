75bf11b48b1b6a34c5a6154502afe7cf28cbd445
BA <laugholouder@gmail.com>
2021-10-07 19:57:43 +0800

    Tweaking regex

--- cli/songlibrarian/regexp.go
+++ cli/songlibrarian/regexp.go
@@ -4,7 +4,7 @@ import "github.com/dlclark/regexp2"
 
 const urlRegexCount = 1
 var regexYoutubeUrl_u0 = regexp2.MustCompile(`^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$`, 0)
-var regexCover_s0 = regexp2.MustCompile(`cover|うたてみた|うたってみた|歌ってみた|踊ってみた|翻唱|翻唱|カバ|試著唱了|试着唱了`, regexp2.IgnoreCase)
+var regexCover_s0 = regexp2.MustCompile(`cover|うたてみた|うたってみた|歌ってみた|歌みた|踊ってみた|翻唱|翻唱|カバ|試著唱了|试着唱了`, regexp2.IgnoreCase)
 var regexOriginal_s1 = regexp2.MustCompile(`original(?!\s?mv)|オリジナル|原創|music video|full mv|mv|official|feat\.`, regexp2.IgnoreCase)
 var regexStream_s2 = regexp2.MustCompile(`sing|stream|歌枠(?!切り抜き)|歌回|歌回|うたう|歌う|歌います|歌配信|弾き語り|お歌|歌ったり|karaoke`, regexp2.IgnoreCase)
 
