2437736caa2043cf8104d0a8cac0dfef326be4bd
BA <laugholouder@gmail.com>
2021-10-11 22:13:29 +0800

    Regex

--- cli/songlibrarian/regexp.go
+++ cli/songlibrarian/regexp.go
@@ -3,16 +3,13 @@ package main
 import "github.com/dlclark/regexp2"
 
 const urlRegexCount = 1
-var regexCover_s0 = regexp2.MustCompile(`cover|うた.?てみた|歌ってみた|歌みた|踊ってみた|翻唱|翻唱|カバ|試著唱了|试着唱了`, regexp2.IgnoreCase)
 var regexYoutubeUrl_u0 = regexp2.MustCompile(`^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)(?!c|post)([\w\-]+)(\S+)?$`, 0)
+var regexCover_s0 = regexp2.MustCompile(`cover|うた.?てみた|歌ってみた|歌みた|踊ってみた|翻唱|翻唱|カバ|試著唱了|试着唱了|\sver\.`, regexp2.IgnoreCase)
 var regexOriginal_s1 = regexp2.MustCompile(`original(?!\s?mv)|オリジナル|原創|music video|mv|official|feat\.|new single`, regexp2.IgnoreCase)
-var regexStream_s2 = regexp2.MustCompile(`【SING】|singing|stream|(うた|歌).{0,3}(れんしゅう|練)|歌枠(?!切り抜き)|うたわく|うた(！|。)|歌回|歌回|うたう|歌う|歌い.?ま.?す|歌配信|弾き語り|お歌|歌ったり|karaoke|(うた|歌|sing).*(guerilla|ゲリラ)|(guerilla|ゲリラ).*(うた|歌|sing)|生.{0,6}(Live|ライブ)`, regexp2.IgnoreCase)
+var regexStream_s2 = regexp2.MustCompile(`【SING】|singing|stream|(うた|歌).{0,3}(れんしゅう|練)|歌枠(?!切り抜き)|うたわく|うた(！|。)|歌回|歌回|うたう|歌う|歌い.?ま.?す|歌配信|弾き語り(?!\s?ver)|お歌|歌ったり|karaoke|(うた|歌|sing).*(guerilla|ゲリラ)|(guerilla|ゲリラ).*(うた|歌|sing)|生.{0,6}(Live|ライブ|放送)\s`, regexp2.IgnoreCase)
 
 
 var regexClips = regexp2.MustCompile(`clip|切り抜き|translate|translation|烤肉|剪輯|切片|譯|精華|剪辑|译|精华`, regexp2.IgnoreCase)
-// var regexBadForCover = regexp2.MustCompile(`original\s?mv`, regexp2.IgnoreCase)
-// var regexBadForOriginal = regexp2.MustCompile(`original\s?mv`, regexp2.IgnoreCase)
-// var regexBadForStream = regexp2.MustCompile(`original\s?mv`, regexp2.IgnoreCase)
 
 var regexUrlMapping = []*regexp2.Regexp {
 	regexYoutubeUrl_u0,
@@ -24,7 +21,7 @@ var regexSubjectMapping = []*regexp2.Regexp {
 	regexStream_s2,
 }
 
-var regexBadForOriginal = regexp2.MustCompile(`cover`, regexp2.IgnoreCase)
+var regexBadForOriginal = regexp2.MustCompile(`cover|てみた|(original|オリジナル)\s?mv.+(てみた|cover)|(てみた|cover).+(original|オリジナル)\s?mv`, regexp2.IgnoreCase)
 var regexBadForCover = regexp2.MustCompile(`live`, regexp2.IgnoreCase)
 var regexBadForStream = regexp2.MustCompile(`debut|birthday stream|食べ`, regexp2.IgnoreCase)
 
