540c302852692791a990f263a230debb262cb4ca
BA <laugholouder@gmail.com>
2021-10-19 22:11:09 +0800

    Regex

--- cli/songlibrarian/regexp.go
+++ cli/songlibrarian/regexp.go
@@ -3,19 +3,19 @@ package main
 import "github.com/dlclark/regexp2"
 
 const urlRegexCount = 1
-var regexYoutubeUrl_u0 = regexp2.MustCompile(`^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)(?!c|post)([\w\-]+)(\S+)?$`, 0)
-var regexCover_s0 = regexp2.MustCompile(`cover|うたっ?てみた|歌ってみた|歌みた|踊ってみた|翻唱|翻唱|カバ|試著唱了|试着唱了|\sver\.`, regexp2.IgnoreCase)
-var regexOriginal_s1 = regexp2.MustCompile(`original(?!\s?mv)|オリジナル|原創|music video|mv|official|feat\.|new single`, regexp2.IgnoreCase)
-var regexStream_s2 = regexp2.MustCompile(`【(sing|歌)】|歌＆|歌(と|＆)(ピアノ|雑談|作業|演奏)|(ピアノ|雑談|作業|演奏)と歌|sing a song|singing|stream|(うた|歌).{0,3}(れんしゅう|練)|朝歌|歌枠(?!切り抜き)|うたわく|】(sing|歌|うた)(！|。|!|\.)|^(sing|歌|うた)(！|。|!|\.)|歌回|歌回|うたう|歌う|(うた|歌)い.?ま.?す|歌配信|弾き語り(?!\s?ver)|お歌|歌ったり|.araoke|(うた|歌|sing).*(guerilla|げりら|ゲリラ)|(guerilla|げりら|ゲリラ).*(うた|歌|sing)|生.{0,6}(Live|ライブ|放送)\s`, regexp2.IgnoreCase)
 
+var regexYoutubeUrl_u0 = regexp2.MustCompile(`^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)(?!c|post)([\w\-]+)(\S+)?$`, 0)
+var regexCover_s0 = regexp2.MustCompile(`cover|うたっ?てみた|歌ってみた|歌みた|踊ってみた|翻唱|翻唱|カバ|試著唱了|试着唱了|\sver\.|arrange ver`, regexp2.IgnoreCase)
+var regexOriginal_s1 = regexp2.MustCompile(`original(?!\s?mv)|オリジナル|原創|music video|mv|official|feat\.|ft\.|new single`, regexp2.IgnoreCase)
+var regexStream_s2 = regexp2.MustCompile(`【(sing|歌)】|歌＆|歌(と|＆)(ピアノ|雑談|作業|演奏)|(ピアノ|雑談|作業|演奏)と歌|sing a song|singing|stream|(うた|歌).{0,3}(れんしゅう|練)|朝歌|(歌|演奏)枠(?!切り抜き)|うたわく|】(sing|歌|うた)(！|。|!|\.)|^(sing|歌|うた)(！|。|!|\.)|歌回|歌回|うたう|歌う|(うた|歌)い.?ま.?す|歌配信|弾き語り(?!\s?ver)|お歌|歌ったり|.araoke|(うた|歌|sing).*(guerilla|げりら|ゲリラ)|(guerilla|げりら|ゲリラ).*(うた|歌|sing)|生.{0,6}(Live|ライブ|放送)\s`, regexp2.IgnoreCase)
 
 var regexClips = regexp2.MustCompile(`clip|切り抜き|translate|translation|烤肉|剪輯|切片|譯|精華|剪辑|译|精华`, regexp2.IgnoreCase)
 
-var regexUrlMapping = []*regexp2.Regexp {
+var regexUrlMapping = []*regexp2.Regexp{
 	regexYoutubeUrl_u0,
 }
 
-var regexSubjectMapping = []*regexp2.Regexp {
+var regexSubjectMapping = []*regexp2.Regexp{
 	regexCover_s0,
 	regexOriginal_s1,
 	regexStream_s2,
