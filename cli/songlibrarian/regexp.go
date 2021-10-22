8109d08919c71db32df033a177eabb0e51b19d79
BA <laugholouder@gmail.com>
2021-10-23 04:05:17 +0800

    ✨ IMPROVE | Regex

--- cli/songlibrarian/regexp.go
+++ cli/songlibrarian/regexp.go
@@ -5,20 +5,14 @@ import "github.com/dlclark/regexp2"
 const urlRegexCount = 1
 
 var regexYoutubeUrl_u0 = regexp2.MustCompile(`^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)(?!c|post|playlist)([\w\-]+)(\S+)?$`, 0)
-var regexCover_s0 = regexp2.MustCompile(`cover|うたっ?てみた|歌ってみた|歌みた|踊ってみた|翻唱|翻唱|カバ|試著唱了|试着唱了|\sver\.|(arrange\s|弾き語り)\s?ver`, regexp2.IgnoreCase)
-var regexOriginal_s1 = regexp2.MustCompile(`original(?!\s?mv)|オリジナル|原創|music video|mv|official|feat\.|ft\.|new single`, regexp2.IgnoreCase)
-var regexStream_s2 = regexp2.MustCompile(`【(sing|song|歌)】|歌＆|歌(と|＆)(ピアノ|雑談|作業|演奏)|(ピアノ|雑談|作業|演奏)と歌|sing a song|singing|stream|(うた|歌).{0,3}(れんしゅう|練)|朝歌|(歌|演奏)枠(?!切り抜き)|うたわく|】(sing|歌|うた)(！|。|!|\.)|^(sing|歌|うた)(！|。|!|\.)|歌回|歌回|うたう|歌う|(うた|歌)い.?ま.?す|歌配信|弾き語り(?!\s?ver)|お歌|歌ったり|.araoke|(うた|歌|sing).*(guerilla|げりら|ゲリラ)|(guerilla|げりら|ゲリラ).*(うた|歌|sing)|生.{0,6}(Live|ライブ|放送)\s|mini concert`, regexp2.IgnoreCase)
+var regexCover_s0 = regexp2.MustCompile(`cover(?!\s?live)|うたっ?てみた|歌いました|歌ってみた|歌みた|踊ってみた|翻唱|翻唱|カバ|試著唱了|试着唱了|\sver\.|(arrange\s|弾き語り)\s?ver`, regexp2.IgnoreCase)
+var regexOriginal_s1 = regexp2.MustCompile(`original(?!\s?mv)|オリジナル|原創|music video|mv|official|feat\.|ft\.|new single|M.?V.+from.+album`, regexp2.IgnoreCase)
+var regexStream_s2 = regexp2.MustCompile(`【(sing|song|歌)】|歌(と|＆|＆)(ピアノ|雑談|作業|演奏)|(ピアノ|雑談|作業|演奏)(と|＆)歌|sing a song|(sing|song|歌).+stream|(うた|歌).{0,3}(れんしゅう|練)|朝歌|(歌|演奏).{0,2}枠|うたわく|(歌|うた)(！|。)|歌回|歌回|うたう|歌う|(うた|歌)い.?ま.?す|歌.{0,4}配信|弾き語り(?!\s?ver)|お歌|歌ったり|.araoke|カラオケ|(うた|歌|sing).*(guerilla|げりら|ゲリラ)|(guerilla|げりら|ゲリラ).*(うた|歌|sing)|生.{0,6}(Live|ライブ|放送)\s|mini concert|耐久.{0,12}(曲|うた|歌|sing)|(曲|うた|歌|sing).{0,12}耐久|cover Live`, regexp2.IgnoreCase)
 
 var regexClips = regexp2.MustCompile(`clip|切り抜き|translate|translation|烤肉|剪輯|切片|譯|精華|剪辑|译|精华`, regexp2.IgnoreCase)
 
 var regexUrlMapping = []*regexp2.Regexp{
 	regexYoutubeUrl_u0,
-}
-
-var regexSubjectMapping = []*regexp2.Regexp{
-	regexCover_s0,
-	regexOriginal_s1,
-	regexStream_s2,
 }
 
 var regexBadForOriginal = regexp2.MustCompile(`cover|てみた|(original|オリジナル)\s?mv.+(てみた|cover)|(てみた|cover).+(original|オリジナル)\s?mv|公開します`, regexp2.IgnoreCase)
@@ -27,6 +21,6 @@ var regexBadForStream = regexp2.MustCompile(`debut|birthday stream|食べ`, rege
 var regexBadForAll = regexp2.MustCompile(`trailer`, regexp2.IgnoreCase)
 
 var regexNamedCover = regexp2.MustCompile(`ver.EMA`, regexp2.IgnoreCase)
-var regexNamedStream = regexp2.MustCompile(`【YouTube Live】波羅ノ鬼 - Harano oni`, regexp2.IgnoreCase)
+var regexNamedStream = regexp2.MustCompile(`【YouTube Live】波羅ノ鬼 - Harano oni|響歌シノのヒビカセ`, regexp2.IgnoreCase)
 
 var regexMention = regexp2.MustCompile(`^<@!(\d+)>`, regexp2.IgnoreCase)
